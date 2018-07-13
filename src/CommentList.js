6778f74e4e6ce2e464df8a045b75d2d392ac23d2
wanghongkun <wanghongkun@corp.netease.com>
2018-07-13 16:13:20 +0800

    初始化项目; 包含第一阶段和第二阶段的全部代码

---
+++ src/CommentList.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react'
+import Comment from './Comment'
+
+class CommentList extends Component {
+  static defaultProps = {
+    comments: []
+  }
+
+  handleDeleteComment = (index) => {
+    if (this.props.onDeleteComment) {
+      this.props.onDeleteComment(index)
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          this.props.comments.map((comment, i) => {
+            return (
+              <Comment
+                key={i}
+                comment={comment}
+                index={i}
+                onDeleteComment={this.handleDeleteComment}
+              />
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
+
+export default CommentList
